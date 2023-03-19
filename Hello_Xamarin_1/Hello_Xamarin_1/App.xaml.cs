810439d78fbe48b70657b1e7c32d1f6fda667138
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2023-03-19 19:35:58 +0100

    Relative layout in XAML

--- Hello_Xamarin_1/Hello_Xamarin_1/App.xaml.cs
+++ Hello_Xamarin_1/Hello_Xamarin_1/App.xaml.cs
@@ -10,7 +10,7 @@ namespace Hello_Xamarin_1
         {
             InitializeComponent();
 
-            MainPage = new AbsolutePage();
+            MainPage = new RelativePage();
         }
 
         protected override void OnStart()
