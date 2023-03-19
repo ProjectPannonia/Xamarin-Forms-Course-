6ad945ce42dd68f42b0a6d48f9536ff4b70dbb47
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2023-03-19 08:12:39 +0100

    Add project files.

---
+++ Hello_Xamarin_1/Hello_Xamarin_1/GreetPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Hello_Xamarin_1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GreetPage : ContentPage
+    {
+        public GreetPage()
+        {
+            InitializeComponent();
+            slider.Value = 0.5;
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            label.Text = String.Format(" Value is {0:F2}", e.NewValue);   
+        }
+
+        
+    }
+}
