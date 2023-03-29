e982e59c715c9c14f35f962a394674be62823f6c
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2023-03-29 20:30:09 +0200

    Image gallery exercise finished

--- Hello_Xamarin_1/Hello_Xamarin_1/PhotoGalleryExecise.xaml.cs
+++ Hello_Xamarin_1/Hello_Xamarin_1/PhotoGalleryExecise.xaml.cs
@@ -12,9 +12,50 @@ namespace Hello_Xamarin_1
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class PhotoGalleryExecise : ContentPage
 	{
+        string[] imagePath;
+        int counter;
 		public PhotoGalleryExecise ()
 		{
 			InitializeComponent ();
+            
+            counter = 0;
+            imagePath = new string[] {
+                "https://www.citibank.com/icg/sa/emea/hungary/assets/img/content/home/200-year.jpg",
+                "https://www.citibank.com/icg/sa/emea/hungary/assets/img/content/home/csc-budapest.jpg",
+                "https://www.fintechfutures.com/files/2018/11/Citi.jpg",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDXEFLGj0ZasSOHfklOahnyugG4z7WxyZQLQ&usqp=CAU"
+
+            };
 		}
-	}
+
+        private void left_Clicked(object sender, EventArgs e)
+        {
+            if (counter == 0)
+                counter = imagePath.Length-1;
+            else
+                counter--;
+            var imageSource = new UriImageSource
+            {
+                
+                Uri = new Uri(imagePath[counter])
+            };
+
+            imageContainer.Source = imageSource;
+            
+        }
+
+        private void right_Clicked(object sender, EventArgs e)
+        {
+            if (counter >= imagePath.Length - 1)
+                counter = 0;
+            else
+                counter++;
+            var imageSource = new UriImageSource
+            {
+                Uri = new Uri(imagePath[counter])
+            };
+
+            imageContainer.Source = imageSource;
+        }
+    }
 }
